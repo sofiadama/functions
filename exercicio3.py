69ba5d3a1d642a84e89918ac792dbfb4aac83bea
SOFIA <sofiadamasin@gmail.com>
2024-10-18 13:02:13 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -1,29 +1,16 @@
-def ajuste(salario):
-    if salario <= 400:
-        novo_salario = salario + salario*0.15
-        aumento = salario*0.15
-        percentual = 15
-    elif salario <= 800:
-        novo_salario = salario + salario*0.12
-        aumento = salario*0.12
-        percentual = 12
-    elif salario <= 1200:
-        novo_salario = salario + salario*0.10
-        aumento = salario*0.10
-        percentual = 10
-    elif salario <= 2000:
-        novo_salario = salario + salario*0.07
-        aumento = salario*0.07
-        percentual = 7
-    elif salario > 2000:
-        novo_salario = salario + salario*0.04
-        aumento = salario*0.04
-        percentual = 4
-
+def ajuste_salarial(salario):
+    faixas = [(2000, 0.04),(1200, 0.07),(800, 0.10),(400, 0.12),(0, 0.15)]
+    
+    for limite, percentual in faixas:
+        if salario > limite:
+            aumento = salario * percentual
+            novo_salario = salario + aumento
+            break
+    
+    print()
     print(f"Novo salário: R$ {novo_salario:.2f}")
     print(f"Reajuste de: R$ {aumento:.2f}")
-    print(f"Em percentual: {percentual} %")
-        
-salario = float(input("Informe seu salário: R$ "))
+    print(f"Em percentual: {percentual*100:.0f}%")
 
-ajuste(salario)
+salario = float(input("Informe seu salário: R$ "))
+ajuste_salarial(salario)
